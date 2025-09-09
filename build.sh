010a57dd25c2961f93333b6da8be13ccdd3f02e7
praveenkumar5151 <thupilipraveenkumar@gmail.com>
2025-09-09 19:18:48 +0530

    added changes

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing dependencies with legacy peer deps..."
-npm install --legacy-peer-deps --no-audit --no-fund
-
-echo "Building the application..."
-npm run build
