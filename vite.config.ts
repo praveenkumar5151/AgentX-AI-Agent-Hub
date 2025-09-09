ddb142b9feb6e95b92f2f610336a1af1d3412816
praveenkumar5151 <thupilipraveenkumar@gmail.com>
2025-09-09 19:50:52 +0530

    added changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -12,6 +13,12 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      plugins: [react()],
+      build: {
+        outDir: 'dist',
+        sourcemap: false,
+        minify: 'esbuild'
       }
     };
 });
